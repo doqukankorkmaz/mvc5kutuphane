3a2e9133ca2e0e7ee48dfe2a51cc963e13eef7ef
doğukan KORKMAZ <dogukankorkmaz42@hotmail.com>
2023-10-31 21:16:17 +0300

    kategori ekle, guncelle ve sil kısımları eklendi
    kategori ekle butonuna basıldığında direkt olarak boş bir kategori ekleme sorunu httpget - httpost komutları ile düzeltildi. action result lar ile birlikte kategori guncelle ve kategori sil kısımları aktif hale getirildi. guncelle butonuna basılınca ıd değerimizin değerleri gelmekte ve içerisindeki kutucukta herhangibir değişiklik olduğunda veritabanında da değiştirerek ana sayfaya yönderiyor ve kategori içeriği değişiyor. sil butonuna basıldığındada  ise direkt kategori içeriği sillinmektedir

--- mvckutuphane/mvckutuphane/Controllers/KategoriController.cs
+++ mvckutuphane/mvckutuphane/Controllers/KategoriController.cs
@@ -15,11 +15,36 @@ namespace mvckutuphane.Controllers
             var degerler=db.TBLKATEGORI.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult KategoriEkle() 
+        {
+            return View();
+        }
+        [HttpPost]
         public ActionResult KategoriEkle(TBLKATEGORI P) 
         {
             db.TBLKATEGORI.Add(P);
             db.SaveChanges();
             return View();
+        }
+        public ActionResult KategoriSil(int id)
+        {
+            var Kategori=db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(Kategori); 
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TBLKATEGORI.Find(id);
+            return View("KategoriGetir", ktg);
+        }
+        public ActionResult KategoriGuncelle(TBLKATEGORI p)
+        {
+            var ktg = db.TBLKATEGORI.Find(p.ID);
+            ktg.AD=p.AD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
