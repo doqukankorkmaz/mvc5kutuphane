81a63069b5cc510c59397e41708e95c988881c2c
doğukan KORKMAZ <dogukankorkmaz42@hotmail.com>
2023-10-31 19:52:52 +0300

    kategoriler kısmı eklendi
    kategoriler kısmını bir foreach döngüsü ile veritabanındaki kayıtların hepsi bir tablo içerisine getirildi. tablo içerisine güncelle ve sil butonları eklendi. tablonun en alt kısmına yeni kategori ekleme kısmı eklendi

--- mvckutuphane/mvckutuphane/Controllers/KategoriController.cs
+++ mvckutuphane/mvckutuphane/Controllers/KategoriController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using mvckutuphane.Models.Entity;
 namespace mvckutuphane.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
+            var degerler=db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        public ActionResult KategoriEkle(TBLKATEGORI P) 
+        {
+            db.TBLKATEGORI.Add(P);
+            db.SaveChanges();
             return View();
         }
     }
