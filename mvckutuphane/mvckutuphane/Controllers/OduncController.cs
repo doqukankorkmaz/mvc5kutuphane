55a04695fd6b16a2e9601bf23ba01af02e912b9d
doğukan KORKMAZ <dogukankorkmaz42@hotmail.com>
2023-12-14 18:28:01 +0300

    ödünç kitap alma ve verme işlemleri
    ödünç kitap verme sayfa tasarımı yapıldı,
    ödünç kitap verme işlemleri yapıldı fakat veritabanına kayıt oluşturuken tarih kısımları veritabanına kayıt edilemiyor ilerleyen süreçte hata çözümleri yapılacaktır,
    iade sayfası tasararımı yapıldı

---
+++ mvckutuphane/mvckutuphane/Controllers/OduncController.cs
@@ -0,0 +1,35 @@
+﻿using mvckutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace mvckutuphane.Controllers
+{
+    
+    public class OduncController : Controller
+    {
+        DBKUTUPHANEEntities db=new DBKUTUPHANEEntities();
+        // GET: Odunc
+        public ActionResult Index()
+        {
+            var degerler=db.TBLHAREKET.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult OduncVer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult OduncVer(TBLHAREKET P)
+        {
+            db.TBLHAREKET.Add(P);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
