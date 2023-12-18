7f21d8a1037ac37920dd15a88632147a65036539
doğukan KORKMAZ <dogukankorkmaz42@hotmail.com>
2023-12-18 19:59:19 +0300

    vitrin paneli yapıldı2
    sayfa olduğu için içerik kısmı ve başlakları Türkçe Hale Getirildi
    Hakkımızda kısmının oluşturulması için hakkımızda adında yeni bir veritabanında tablo oluşturuldu.
    yeni sınıf aracılığı ile vitrincontroller buradan çalışacak şekilde tasarlandı
    
    İletişim sayfasının oluşturulabilmesi için sql server aracılığı ile yeni bir iletişim tablosu oluşturuldu
    şu an için iletişim formuna eklenen bilgiler veritabanına gitmemekte ve hata vermekte ilerleyen süreçte hata öçözümü yapılmaya çalışılacaktır

---
+++ mvckutuphane/mvckutuphane/Models/Siniflarim/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mvckutuphane.Models.Entity;
+namespace mvckutuphane.Models.Siniflarim
+{
+    public class Class1
+    {
+        public IEnumerable<TBLKITAP> Deger1 { get; set; }
+        public IEnumerable<TBLHAKKIMIZDA> Deger2 { get; set; }
+    }
+}
